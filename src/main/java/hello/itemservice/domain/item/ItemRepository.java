bee2595db322af24a473ff627b38370f72d938e8
w-garden <w.garden316@gmail.com>
2022-12-20 00:06:47 +0900

    20221219 MVC1편 완료, 타임리프로 입력폼 처리 개발중

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -7,9 +7,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 핵심 도메인 로직
+ */
 @Repository
 public class ItemRepository {
-    private static final Map<Long, Item> store = new HashMap<>();
+    private static final Map<Long, Item> store = new HashMap<>(); //싱글톤 유지 위해 static 선언
     private static long sequence = 0L;
 
     public Item save(Item item) {
@@ -19,12 +22,14 @@ public class ItemRepository {
     }
     public Item findById(Long id){
         return store.get(id);
-    }
-    public List<Item> findAll(){
-        return new ArrayList<>(store.values());
+    } //id 기준조회
+    public List<Item> findAll(){ //전체 조회
+        return new ArrayList<>(store.values()); //한번 감싸서 넘김
     }
 
-    public void update(Long itemId, Item updateParam){
+    public void update(Long itemId, Item updateParam){ //수정 메서드
+        //ItemParamDTO를 만드는 것을 더 추천
+
         Item findItem = findById(itemId);
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
