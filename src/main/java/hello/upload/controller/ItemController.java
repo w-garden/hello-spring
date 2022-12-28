5f9c4f5e0e25a0857095170383bdcb93e8e072a1
w-garden <w.garden316@gmai.com>
2022-12-28 19:27:23 +0900

    20221228 파일 업로드 기능 개발완료

---
+++ src/main/java/hello/upload/controller/ItemController.java
@@ -0,0 +1,88 @@
+package hello.upload.controller;
+
+import hello.upload.domain.Item;
+import hello.upload.domain.ItemRepository;
+import hello.upload.domain.UploadFile;
+import hello.upload.file.FileStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form) {
+        return "item-form";
+    }
+
+    @PostMapping("/items/new")
+    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) throws IOException {
+
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+        List<UploadFile> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
+
+        //데이터 베이스에 저장
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeImageFiles);
+        itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/items/{id}")
+    public String items(@PathVariable Long id, Model model) {
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item", item);
+        return "item-view";
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        //file: 이 붙으면 내부 파일에 접근한다
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}", uploadFileName);
+
+        //한글이 깨질 수 있으니 작업
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+        return ResponseEntity.ok()
+                //헤더에 다음 내용을 추가해야 다운로드 할 수 있음
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
+    }
+}
