2ce566af4da7188a02098a6ebcd95c069a2ecda1
w-garden <w.garden316@gmai.com>
2022-12-29 19:11:15 +0900

    20221229 트랜잭션

---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,63 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection conn = dataSource.getConnection();
+        try{
+            conn.setAutoCommit(false); //트랜잭션 시작
+            bizLogic(conn, fromId, toId, money);  //비즈니스 로직
+            conn.commit(); //성공시 커밋
+        }catch (Exception e){
+            conn.rollback(); //실패시 롤백
+            throw new IllegalStateException(e);
+        }finally {
+            release(conn);
+        }
+    }
+
+    private void bizLogic(Connection conn, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(conn, fromId);
+        Member toMember = memberRepository.findById(conn, toId);
+
+        memberRepository.update(conn, fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(conn, toId, toMember.getMoney() + money);
+    }
+
+    private void release(Connection conn) {
+        if(conn !=null){
+            try{
+                conn.setAutoCommit(true); //다시 원래대로 돌려놓고
+                conn.close();
+            }catch (Exception e){
+                log.info("error", e);
+            }
+        }
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생헀습니다!");
+        }
+    }
+
+
+}
