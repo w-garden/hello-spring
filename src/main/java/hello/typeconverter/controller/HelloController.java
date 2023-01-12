8efc2f14274d863cd9a07f78e2e0af0744b63e5d
w-garden <w.garden316@gmail.com>
2023-01-12 22:02:35 +0900

    20230112 뷰 템플릿에 컨버터 적용하기

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,4 +26,11 @@ public class HelloController {
         return "ok";
     }
 
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort){
+        System.out.println("ipPort IP : " + ipPort.getIp());
+        System.out.println("ipPort PORT : " + ipPort.getPort());
+        return "ok";
+    }
 }
