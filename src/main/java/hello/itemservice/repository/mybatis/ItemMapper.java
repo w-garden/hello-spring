9dd2d8c61bed63bc034da8b6fc8e8dcfecd60a84
w-garden <w.garden316@gmail.com>
2023-01-18 23:10:38 +0900

    20230118 JPA 세팅 및 실행

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -10,6 +10,10 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ *  이 인터페이스의 메서드를 호출하면  .xml 의 해당 SQL을 실행하고 결과를 돌려준다
+ */
 @Mapper
 public interface ItemMapper {
 
