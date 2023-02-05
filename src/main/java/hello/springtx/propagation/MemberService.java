37bec5b878e3927da7dab925fa7fb52c02a2aa59
w-garden <w.garden316@gmail.com>
2023-02-05 23:44:34 +0900

    20230205 스프링 트랜잭션 propagation

--- src/main/java/hello/springtx/propagation/MemberService.java
+++ src/main/java/hello/springtx/propagation/MemberService.java
@@ -3,6 +3,7 @@ package hello.springtx.propagation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -11,6 +12,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final LogRepository logRepository;
 
+    @Transactional
     public void joinV1(String username){
         Member member = new Member(username);
         Log logMessage = new Log(username);
@@ -20,10 +22,12 @@ public class MemberService {
         memberRepository.save(member);
         log.info("== memberRepository 호출 종료 ==");
 
-        log.info("== memberRepository 호출 시작 ==");
+        log.info("== logRepository 호출 시작 ==");
         logRepository.save(logMessage);
-        log.info("== memberRepository 호출 종료 ==");
+        log.info("== logRepository 호출 종료 ==");
     }
+
+    @Transactional
     public void joinV2(String username){
         Member member = new Member(username);
         Log logMessage = new Log(username);
@@ -33,13 +37,13 @@ public class MemberService {
         memberRepository.save(member);
         log.info("== memberRepository 호출 종료 ==");
 
-        log.info("== memberRepository 호출 시작 ==");
+        log.info("== logRepository 호출 시작 ==");
         try{
             logRepository.save(logMessage);
         }catch (RuntimeException e){
             log.info("log 저장에 실패 했습니다. logMessage ={}",logMessage.getMessage());
             log.info("정상 흐름 반환");
         }
-        log.info("== memberRepository 호출 종료 ==");
+        log.info("== logRepository 호출 종료 ==");
     }
 }
