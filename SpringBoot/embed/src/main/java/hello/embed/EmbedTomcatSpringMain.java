eac73da1a8b373441fe151bf0bd7f2efeeea9b46
w-garden <w.garden316@gmail.com>
2023-08-15 23:57:33 +0900

    내장톰캣

--- SpringBoot/embed/src/main/java/hello/embed/EmbedTomcatSpringMain.java
+++ SpringBoot/embed/src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -1,27 +1,33 @@
 package hello.embed;
 
-import hello.servlet.HelloServlet;
+import hello.spring.HelloConfig;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.File;
 
-public class EmbedTomcatServletMain {
+public class EmbedTomcatSpringMain {
     public static void main(String[] args) throws LifecycleException {
-        System.out.println("EmbedTomcatServletMain.main");
+        System.out.println("EmbedTomcatSpringMain.main");
         //톰캣 설정
         Tomcat tomcat = new Tomcat();
         Connector connector = new Connector();
         connector.setPort(8080);
         tomcat.setConnector(connector);
 
+        //스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class);
 
+        //스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
+        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
 
-        //서블릿 등록
+        //디스패터 서블릿 등록
         Context context = tomcat.addContext("", "/");
-
         //== 코드 추가 시작==
         File docBaseFile = new File(context.getDocBase());
         if (!docBaseFile.isAbsolute()) {
@@ -31,8 +37,9 @@ public class EmbedTomcatServletMain {
 
         //== 코드 추가 종료==
 
-        tomcat.addServlet("", "helloServlet", new HelloServlet());
-        context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+        tomcat.addServlet("", "dispatcher", dispatcher);
+        context.addServletMappingDecoded("/", "dispatcher");
+
         tomcat.start();
     }
 }
