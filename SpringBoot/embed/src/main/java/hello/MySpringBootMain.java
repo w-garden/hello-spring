ff0d85f547fd1f457f4f028c36deed23d70be67b
w-garden <w.garden316@gmail.com>
2023-08-16 23:56:56 +0900

    스프링부트 클래스 만들기

--- SpringBoot/embed/src/main/java/hello/MySpringBootMain.java
+++ SpringBoot/embed/src/main/java/hello/MySpringBootMain.java
@@ -7,6 +7,8 @@ import hello.boot.MySpringBootApplication;
 public class MySpringBootMain {
     public static void main(String[] args) {
         System.out.println("MySpringBootMain.main");
+
         MySpringApplication.run(MySpringBootMain.class, args);
+
     }
 }
