7cbecbe2404f9cc72d4826da90e6621539c4684e
w-garden <w.garden316@gmail.com>
2023-08-21 01:39:43 +0900

    Auto Configuration

---
+++ SpringBoot/autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -0,0 +1,27 @@
+package hello.config;
+
+import memory.MemoryCondition;
+import memory.MemoryController;
+import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@Conditional(MemoryCondition.class)
+//MemoryCondition 반환 결과가 true일 경우에만 bean등록됨
+
+
+@ConditionalOnProperty(name="memory", havingValue = "sn")
+public class MemoryConfig {
+    @Bean
+    public MemoryController memoryController(){
+        return new MemoryController(memoryFinder());
+    }
+    @Bean
+    public MemoryFinder memoryFinder(){
+        return new MemoryFinder();
+    }
+
+}
