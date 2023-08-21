71aefc565a719ccb52b81478fa23fc1052c322d1
w-garden <w.garden316@gmail.com>
2023-08-21 22:59:40 +0900

    순수 라이브러리 만들어서 사용하기

---
+++ SpringBoot/memory-v1/src/test/java/memory/MemoryFinderTest.java
@@ -0,0 +1,18 @@
+package memory;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MemoryFinderTest {
+    @Test
+    void get(){
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        assertThat(memory).isNotNull();
+    }
+
+
+}
