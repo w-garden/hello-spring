daabd62678ef5138cfb6d6d309887dd5cbba86b8
w-garden <w.garden316@gmail.com>
2023-09-06 22:57:17 +0900

    액츄에이터와 보안

--- SpringBoot/actuator/src/main/java/hello/ActuatorApplication.java
+++ SpringBoot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ActuatorApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
     }
