b6979a04c60424cd45d038ae6757e2906f11e65c
w-garden <w.garden316@gmail.com>
2024-04-10 21:44:56 +0900

    인터페이스 기반 프록시

---
+++ advanced_proxy/src/main/java/hello/proxy/trace/ThreadLocalLogTrace.java
@@ -0,0 +1,73 @@
+package hello.proxy.trace;
+
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+
+import static hello.proxy.util.PrefixConstant.*;
+
+@Slf4j
+public class ThreadLocalLogTrace implements LogTrace {
+    private final ThreadLocal<TraceId> traceHolder = new ThreadLocal<>();
+
+    @Override
+    public TraceStatus begin(String message) {
+        syncTraceId();
+        TraceId traceId = traceHolder.get();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+
+    @Override
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+
+    @Override
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+        releaseTraceId();
+    }
+
+    private void syncTraceId() {
+        TraceId traceId = traceHolder.get();
+        if (traceId == null) {
+            traceHolder.set(new TraceId());
+        } else {
+            traceHolder.set(traceId.createNextId());
+        }
+    }
+
+    private void releaseTraceId() {
+        TraceId traceId = traceHolder.get();
+
+        if (traceId.isFirstLevel()) {
+            traceHolder.remove();
+        } else {
+            traceHolder.set(traceId.createPreviousId());
+        }
+    }
+
+
+    private Object addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
+}
---
+++ advanced_proxy/src/main/java/hello/proxy/trace/TraceStatus.java
@@ -0,0 +1,17 @@
+package hello.proxy.trace;
+
+import lombok.Getter;
+
+//로그 시작 시  상태 정보
+@Getter
+public class TraceStatus {
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+
+    public TraceStatus(TraceId traceId, Long startTimeMs, String message) {
+        this.traceId = traceId;
+        this.startTimeMs = startTimeMs;
+        this.message = message;
+    }
+}
