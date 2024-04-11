80981c0c11ef39792ef7ab41c666c83a268a084b
w-garden <w.garden316@gmail.com>
2024-04-11 22:24:56 +0900

    클래스 기반 프록시 패턴

---
+++ advanced_proxy/src/test/java/hello/proxy/concreteproxy/code/ConcreteClient.java
@@ -0,0 +1,13 @@
+package hello.proxy.concreteproxy.code;
+
+public class ConcreteClient {
+    private final ConcreteLogic concreteLogic;
+
+    public ConcreteClient(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+
+    public void execute(){
+        concreteLogic.operation();
+    }
+}
