06ec98e9598e0612b0f045714c8425191475e6b6
w-garden <w.garden316@gmail.com>
2024-04-20 22:55:28 +0900

    스프링이 제공하는 빈후처리기

--- advanced_proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ advanced_proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -8,6 +8,8 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.ThreadLocalLogTrace;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,13 +17,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+/*
+http://localhost:8080/v1/request?itemId=hello
+http://localhost:8080/v2/request?itemId=hello
+http://localhost:8080/v3/request?itemId=hello
+
+http://localhost:8080/v1/no-log
+ */
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfigV2.class)
+//@Import(ProxyFactoryConfigV2.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
 public class ProxyApplication {
 
