e572bb73d2100989c650949972f9155c597f18f6
w-garden <w.garden316@gmail.com>
2024-04-21 19:54:36 +0900

    @Aspect 애노테이션으로 프록시 구현하기

--- advanced_proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ advanced_proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -10,6 +10,7 @@ import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_app.AppConfig;
 import hello.proxy.trace.ThreadLocalLogTrace;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.boot.SpringApplication;
@@ -32,7 +33,8 @@ http://localhost:8080/v1/no-log
 //@Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AppConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
 public class ProxyApplication {
 
---
+++ advanced_proxy/src/main/java/hello/proxy/config/v6_app/aspect/LogTraceAspect.java
@@ -0,0 +1,39 @@
+package hello.proxy.config.v6_app.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+@Aspect
+public class LogTraceAspect {
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Around("execution(* hello.proxy.app..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        //ProceedingJoinPoint joinPoint 에는 내부에 호출 대상, 전달 인자, 그리고 어떤 객체와 어떤 메서드가 호출되었는지 정보가 포함되어 있다.
+        TraceStatus status = null;
+        try {
+            String message = joinPoint.getSignature().toShortString();
+
+            status = logTrace.begin(message);
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }    }
+}
+
