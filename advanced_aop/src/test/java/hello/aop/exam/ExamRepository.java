6eafc8f27db78e1078712a78a55555ce1f9df18d
w-garden <w.garden316@gmail.com>
2024-04-27 01:01:05 +0900

    AOP 예제

---
+++ advanced_aop/src/test/java/hello/aop/exam/ExamRepository.java
@@ -0,0 +1,28 @@
+package hello.aop.exam;
+
+import hello.aop.exam.annotation.Retry;
+import hello.aop.exam.annotation.Trace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExamRepository {
+    private static final Logger log = LoggerFactory.getLogger(ExamRepository.class);
+    private static int seq = 0;
+
+    /**
+     * 5번에 1번 실패하는 요청 만들기
+     */
+    @Trace
+    @Retry(value = 4)
+    public String save(String itemId) {
+        seq++;
+        log.info("seq = {}", seq);
+
+        if (seq % 5 == 0) {
+            throw new IllegalStateException("예외 발생");
+        }
+        return itemId;
+    }
+}
