556e0a224043887ac56eda6380c88018f05ad38c
w-garden <w.garden316@gmail.com>
2024-04-27 01:42:59 +0900

    프록시의 한계와 대안

---
+++ advanced_aop/src/test/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,30 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * ObjectProvider(Provider), ApplicationContext를 사용해서 지연(LAZY) 조회
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV2 {
+
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+
+    public void external() {
+        log.info("call external");
+        CallServiceV2 callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+
+}
---
+++ advanced_aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,43 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false);
+
+        //프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        log.info("proxy class={}", memberServiceProxy.getClass());
+
+        assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        log.info("proxy class={}", memberServiceProxy.getClass());
+
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+
+    }
+}
